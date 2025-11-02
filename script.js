e5746e6f87affba27caf3806f3ff595e89b088fd
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-02 16:58:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function firstChar(text) {
-    return text.trim().charAt(0); 
+	if (typeof text !== 'string') return '';
+  const trimmed = text.trim();
+  return trimmed.charAt(0);
 }
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
